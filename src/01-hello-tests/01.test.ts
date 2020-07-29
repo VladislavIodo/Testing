87a21024f215cd59e550c98d9bff022537dac92d
IodoV <vladislav_iodo@mail.ru>
2020-07-29 15:37:40 +0300

    first commit

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,14 @@
+// import {sum} from './01';
+
+
+
+// test('sum should be corrrect'. () => {
+// const a = 1;
+// const b = 2;
+// const c = 3;
+
+// const result = sum(a, b);
+
+// epect(result).toBe(3);
+
+// })
